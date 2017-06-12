9f4c983c18be5e76e92da5712df11065e9c632e1
lewi-g <dr.lewi@gmail.com>
2017-06-12 15:37:50 -0400

    7th commit

--- drill1.js
+++ drill1.js
@@ -1,7 +1,10 @@
 function whoAmI(name, age) {
-	
+	if ( name === undefined || age === undefined) { 
+		console.log("Arguments not valid"); 
+	} else {
 	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 	console.log("I was born in " + yearOfBirth(age));
+	}
 }
 
 function yearOfBirth(age){
@@ -9,4 +12,4 @@ function yearOfBirth(age){
 	return 2016 - age;
 }
 
-whoAmI("chris",-29);
+whoAmI();
