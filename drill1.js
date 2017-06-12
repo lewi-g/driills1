fedb3cbe547e5f002270ec09c32a086e983d9447
daviddinnison <david.g.dinnison@gmail.com>
2017-06-12 15:18:19 -0400

    fourth commit

--- drill1.js
+++ drill1.js
@@ -1,5 +1,7 @@
 function whoAmI(name, age) {
+	var yearOfBirth = 2016 - age;
 	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+	console.log(`I was born in ${yearOfBirth}`)
 }
 
 whoAmI("chris","29");
