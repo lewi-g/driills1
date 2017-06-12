7568165e126b03d56882686fae4f9b301ac9f114
lewi-g <dr.lewi@gmail.com>
2017-06-12 15:24:24 -0400

    5th commit

--- drill1.js
+++ drill1.js
@@ -1,7 +1,11 @@
 function whoAmI(name, age) {
-	var yearOfBirth = 2016 - age;
+	var yearOfBirth;
 	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 	console.log(`I was born in ${yearOfBirth}`)
+}
+
+function yearOfBirth(age){
+	return 2016 - age;
 }
 
 whoAmI("chris","29");
