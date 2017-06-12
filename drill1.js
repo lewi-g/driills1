ac3971c5f369d5921496b8b528c03b8dfb99a7ef
daviddinnison <david.g.dinnison@gmail.com>
2017-06-12 15:31:18 -0400

    fifth commit

--- drill1.js
+++ drill1.js
@@ -1,11 +1,12 @@
 function whoAmI(name, age) {
-	var yearOfBirth;
+	
 	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-	console.log(`I was born in ${yearOfBirth}`)
+	console.log("I was born in " + yearOfBirth(age));
 }
 
 function yearOfBirth(age){
+	throw new Error("Age cannot be negative");
 	return 2016 - age;
 }
 
-whoAmI("chris","29");
+whoAmI("chris",-29);
